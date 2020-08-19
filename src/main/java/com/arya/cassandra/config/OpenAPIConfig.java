a13367ef9623ef47b6407549601796d1f4fb275d
rahul-ghadge <rahul.g5557@gmail.com>
2020-08-20 00:27:13 +0530

    API doc config

---
+++ src/main/java/com/arya/cassandra/config/OpenAPIConfig.java
@@ -0,0 +1,22 @@
+package com.arya.cassandra.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info().title("Super Hero API")
+                .version(appVersion)
+                .description("This is a sample CRUD application using spring data and cassandra - a library for OpenAPI 3 with spring boot.")
+                .termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
