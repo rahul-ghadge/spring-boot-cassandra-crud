9b2e5e391af1bf6cc6b6781602f0c2c5934364fb
rahul_ghadage <rahul.g5557@gmail.com>
2020-08-19 18:54:48 +0530

    Bug Fix

--- src/main/java/com/arya/cassandra/config/CassandraConfig.java
+++ src/main/java/com/arya/cassandra/config/CassandraConfig.java
@@ -1,21 +1,30 @@
 package com.arya.cassandra.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
+@EnableCassandraRepositories
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Value("${spring.data.cassandra.keyspace-name: simple_crud}")
     private String KEYSPACE;
+
+    @Value("${spring.data.cassandra.contact-points: localhost}")
+    private String CONTACT_POINT;
+
+    @Value("${spring.data.cassandra.port: 9042}")
+    private int PORT;
 
     //public static final String KEYSPACE = "simple_crud";
 
@@ -34,7 +43,7 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
     protected String getLocalDataCenter() {
-        return "dc1";
+        return "datacenter1";
     }
 
     @Override
@@ -50,5 +59,16 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Override
     public String[] getEntityBasePackages() {
         return new String[]{"com.arya.cassandra.model"};
+    }
+
+
+    @Bean
+    public CassandraClusterFactoryBean cluster() {
+        CassandraClusterFactoryBean cluster = super.cluster();
+
+        cluster.setContactPoints(CONTACT_POINT);
+        cluster.setPort(PORT);
+
+        return cluster;
     }
 }
